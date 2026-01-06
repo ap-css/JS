2f26c8c821f7771e077e7dbd3fca59cc5ff476bd
Abhi <abhinavpatil14062004@gmail.com>
2026-01-06 11:16:27 +0000

    datatypes

---
+++ 01_BASICS/01_DATATYPES.js
@@ -0,0 +1,19 @@
+let name = "abhinav"  // string
+let age = 21  // integer
+let isLoggedIn = true // boolean
+let state;
+
+
+/*
+1. number : 2 to power 53
+2. bigint : used for very large numbers 
+3. string : generally used with ""
+4. booleaan: true or false
+5. null : standalone value
+6. undefined : variable is created but value not assigned
+7. symbol : unique , used in Reactjs
+*/
+
+console.log(typeof undefined); // output : undefined
+console.log(typeof null);      // output : object
+
