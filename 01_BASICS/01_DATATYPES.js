26ac0408e08829b6b782b40229e5fcd5ab595b44
Abhi <abhinavpatil14062004@gmail.com>
2026-01-07 09:40:43 +0000

    datatype

--- 01_BASICS/01_DATATYPES.js
+++ 01_BASICS/01_DATATYPES.js
@@ -4,7 +4,9 @@ let isLoggedIn = true // boolean
 let state;
 
 
-/*
+/* 
+Following are the primitive data types , which are immutable (cannot be altered after creation) and represent single values
+
 1. number : 2 to power 53
 2. bigint : used for very large numbers 
 3. string : generally used with ""
@@ -12,8 +14,28 @@ let state;
 5. null : standalone value
 6. undefined : variable is created but value not assigned
 7. symbol : unique , used in Reactjs
+
 */
 
 console.log(typeof undefined); // output : undefined
 console.log(typeof null);      // output : object
 
+
+// Symbol example
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid);    // false , although values are same but its symbol fundamentals , uniqueness
+
+
+
+/*
+Following are the reference or non-primitive data types, used for dynamic and complex data that do not have a fixed size
+
+1. Array
+2. Objects
+3. Functions
+
+They have return type as Object and Functions have function-object return type
+
+*/
